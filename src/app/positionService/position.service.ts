4b60242c085560dbc162ac4c379c77d2a64aee26
Smith2008S <60987042+Smith2008S@users.noreply.github.com>
2020-10-01 16:16:58 -0500

    Update position.service.ts

--- src/app/positionService/position.service.ts
+++ src/app/positionService/position.service.ts
@@ -1,7 +1,7 @@
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { DataGPS, DataGPSColumns } from '../dataGps.model';
+import { DataGPS, DataGPSColumns } from 'src/app/dataGPS.model';
 import { Observable } from 'rxjs';
 
 @Injectable({
